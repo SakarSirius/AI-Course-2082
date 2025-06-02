87e12639f22094680d1445e25129e1306ca60f9f
SakarSirius <sakar.rayamajhi@gmail.com>
2025-06-02 12:16:07 +0545

    Initial commit of all AI Course materials

---
+++ Week 1/Day 2/day2.py
@@ -0,0 +1,49 @@
+# top 10 natural numbers cube
+for i in range(1,11):
+    print (i*i*i)
+# declaring function to calculate square of a number
+def calculate_square(length):
+    return length*length
+# calling a function
+print("The area of first square is:", calculate_square(6))
+print("The area of second square is:", calculate_square(50))
+
+#listing
+fruits = ["apple", "banana", "mango", "litchi", "watermelon"]
+print(fruits)
+# #accessing elements from list
+# print(fruits[0])
+# print(fruits[4])
+# print(fruits[-1])
+# print(fruits[2])
+# fruits.append("pineapple")
+#
+# print(fruits)
+# fruits.remove("mango")
+# print(fruits)
+
+# Sets and Tuples
+#Home Research
+
+# #Dictionary in Python
+#
+# student = {
+#     "name" : "Ram",
+#     "class" : 7,
+#     "gender" : "male"
+# }
+# # Accessing values from keys in a dictionary
+# print(student["name"])
+# print(student["class"])
+#
+# #updating values of a particular key in a dictionary
+# student["gender"]= "female"
+# print(student)
+#
+# #Adding a new key value pair into the existing dictionary
+# student["roll_no"]=10
+# print(student)
+#
+# #Deleting a key value pair from the existing dictionary
+# del student["class"]
+# print(student)
---
+++ Week 1/README.md
@@ -0,0 +1,2 @@
+"# AI-Course-2082" 
+"# AI-Course-2082" 
